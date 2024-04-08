cbb4caa3534fdd060d1f394b36ad9d478390843d
AliCodesJava <ali.ottawa.canada@gmail.com>
2024-04-07 23:04:15 -0400

    commit part3 work

--- main.py
+++ main.py
@@ -5,26 +5,87 @@ import torch
 
 # Point d'entrée principal du script
 if __name__ == "__main__":
-
     # Définir les répertoires source et de sortie, et le nom de l'image
-    source_path_dir = "examples/source"
-    output_path_dir = "examples/output"
-    image_name = "sample_2.png"
+    source_path_dir = "images/"
+    output_path_dir = "images_output/"
 
-    # Charger le modèle et appliquer les transformations à l'image
-    seg_model, transforms = model.get_model()
+        # get masks for reference people
+    # reference_histograms: [hist1, hist2, ...]
+    # make histograms from reference people masks
+    reference_histograms = []
+    for image_name in os.listdir('reference_people/input'):
+        print(image_name)
+        # Charger le modèle et appliquer les transformations à l'image
+        seg_model, transforms = model.get_model()
 
-    # Ouvrir l'image et appliquer les transformations
-    image_path = os.path.join(source_path_dir, image_name)
-    image = Image.open(image_path)
-    transformed_img = transforms(image)
-    
+        # Ouvrir l'image et appliquer les transformations
+        image_path = os.path.join('reference_people/input', image_name)
+        image = Image.open(image_path)
+        transformed_img = transforms(image)
 
-    # Effectuer l'inférence sur l'image transformée sans calculer les gradients
-    with torch.no_grad():
-        output = seg_model([transformed_img])
+        # Effectuer l'inférence sur l'image transformée sans calculer les gradients
+        with torch.no_grad():
+            output = seg_model([transformed_img])
 
-    # Traiter le résultat de l'inférence
-    result = tools.process_inference(output,image)
-    result.save(os.path.join(output_path_dir, image_name))
-    # result.show()
+        # Traiter le résultat de l'inférence
+        result = tools.process_inference(output, image, os.path.join('reference_people', 'saved_masks', image_name.removesuffix('.png') + '_saved_masks.npy'))
+            
+        result.save(os.path.join('reference_people/output', image_name))
+        # result.show()
+
+    # generating images with masks as per part2
+    # also generating npy files for each image with the masks saved
+    # so we can read them later and do computations using them
+    for feed in ['cam0', 'cam1']:
+        for image_name in os.listdir(os.path.join(source_path_dir, feed)):
+            if image_name in os.listdir(os.path.join(output_path_dir, feed)):
+                continue
+            # Charger le modèle et appliquer les transformations à l'image
+            seg_model, transforms = model.get_model()
+
+            # Ouvrir l'image et appliquer les transformations
+            image_path = os.path.join(source_path_dir, feed, image_name)
+            image = Image.open(image_path)
+            transformed_img = transforms(image)
+
+            # Effectuer l'inférence sur l'image transformée sans calculer les gradients
+            with torch.no_grad():
+                output = seg_model([transformed_img])
+
+            # current path where we save the npy file of masks of the current imag being generated
+            current_saved_masks_path = os.path.join(output_path_dir, feed, 'saved_masks', image_name.removesuffix('.png') + '_saved_masks.npy')
+            # Traiter le résultat de l'inférence
+            result = tools.process_inference(output, image, current_saved_masks_path)
+                
+            ## (optional) apply saved mask
+            # result = tools.apply_saved_mask(image)
+            
+            result.save(os.path.join(output_path_dir, feed, image_name))
+            # result.show()
+
+    results = []
+    corresponding_filename = []
+    # compare each reference person's histogram to the histogram the 'masked' people from part2's RCNN
+    for person_histogram in reference_histograms:
+        for feed in ['cam0', 'cam1']:
+            for mask_filename in os.listdir(os.path.join(output_path_dir, feed, 'saved_masks')):
+                print(os.path.join(output_path_dir, feed, 'saved_masks', mask_filename))
+                current_masks = tools.read_saved_masks(os.path.join(output_path_dir, feed, 'saved_masks', mask_filename))
+                print(current_masks)
+                for mask in current_masks:
+                    print(mask_filename)
+
+                    correlation = 1
+
+                    if len(results) < 100:
+                        results.append(correlation)
+                        corresponding_filename.append(mask_filename)
+                    elif min(results) < correlation:
+                        i = results.index(min(results))
+                        results.remove(min(results))
+                        corresponding_filename.remove(corresponding_filename[i])
+
+                        results.append(correlation)
+                        corresponding_filename.append(mask_filename)
+
+    print(zip(results, corresponding_filename))
--- utils/tools.py
+++ utils/tools.py
@@ -1,10 +1,12 @@
 import numpy as np
 from utils.config import THRESHOLD, PERSON_LABEL, MASK_COLOR, ALPHA
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Fonction pour traiter les sorties d'inférence du modèle
-def process_inference(model_output, image):
-
+def process_inference(model_output, image, path):
+    np_masks = []
     # Extraire les masques, les scores, et les labels de la sortie du modèle
     masks = model_output[0]['masks']
     scores = model_output[0]['scores']
@@ -14,17 +16,90 @@ def process_inference(model_output, image):
     img_np = np.array(image)
 
     # Parcourir chaque prédiction pour appliquer le seuil et le label
-    for i, (mask, score, label) in enumerate(zip(masks, scores, labels)):
+    for _, (mask, score, label) in enumerate(zip(masks, scores, labels)):
     
         # Appliquer le seuil et vérifier si le label correspond à une personne
         if score > THRESHOLD and label == PERSON_LABEL:
             
             # Convertir le masque en tableau numpy et l'appliquer à l'image            
-            mask_np = mask[0].mul(255).byte().cpu().numpy() > (THRESHOLD * 255)            
+            mask_np = mask[0].mul(255).byte().cpu().numpy() > (THRESHOLD * 255) 
+            np_masks.append(mask_np)            
+
             for c in range(3):
                 img_np[:, :, c] = np.where(mask_np, 
                                         (ALPHA * MASK_COLOR[c] + (1 - ALPHA) * img_np[:, :, c]),
                                         img_np[:, :, c])
-    
+                
+    masks_array = np.array(np_masks, dtype=bool)
+    with open(path, 'wb') as f:
+        np.save(f, masks_array)
+        print(f"Saved masks to {path}")
+
+    ## (Optional) save mask
+    # with open('examples/output/saved_masks.npy', 'wb') as f:
+    #     np.save(f,np_masks)
+
     # Convertir en image à partir d'un tableau numpy et le renvoyer            
     return Image.fromarray(img_np.astype(np.uint8))
+
+def apply_saved_mask(image):
+    # Convertir l'image en tableau numpy
+    img_np = np.array(image)
+    masks = np.load('examples/output/saved_masks.npy')
+    # Parcourir chaque prédiction pour appliquer le seuil et le label
+    for _, mask in enumerate(masks):  
+        for c in range(3):
+            img_np[:, :, c] = np.where(mask, 
+                                    (ALPHA * MASK_COLOR[c] + (1 - ALPHA) * img_np[:, :, c]),
+                                    img_np[:, :, c])
+    
+    # Convertir en image à partir d'un tableau numpy et le renvoyer
+    return Image.fromarray(img_np.astype(np.uint8))
+
+# def read_saved_mask():
+#     masks = np.load('images_output/cam0/saved_masks/1637433774251426200_saved_masks.npy')
+#     for mask in masks:
+#         print(mask.astype(np.uint8))
+        
+def read_saved_masks(path):
+    saved_masks = np.load(path)
+    return saved_masks
+    # grayscale_np = np.array(grayscale_image)
+
+    # for idx, person_mask in enumerate(saved_masks):
+    #     segmented_person = grayscale_np * person_mask
+        
+    #     plot_grayscale_histogram(segmented_person, idx + 1)
+
+
+
+    # saved_masks = np.load(path)
+    # print(saved_masks)
+    # # Assuming the first mask corresponds to "person 1" and is of interest
+    # person_1_mask = saved_masks[0]
+
+    # # Convert the original image to grayscale
+    # grayscale_image = Image.open(image_path).convert('L')
+    # grayscale_np = np.array(grayscale_image)
+
+    # # Apply the mask to the grayscale image to segment "person 1"
+    # segmented_person = grayscale_np * person_1_mask
+
+    # # Calculate and plot the grayscale histogram for the segmented area
+    # print(segmented_person.astype(np.uint8))
+    # # plot_grayscale_histogram(segmented_person)
+    # return person_1_mask.astype(np.uint8)
+
+def plot_grayscale_histogram(segmented_person):
+    """
+    Function to calculate and plot the histogram for a grayscale image.
+    """
+    histogram, bin_edges = np.histogram(
+        segmented_person[segmented_person > 0], bins=256, range=(0, 255)
+    )
+    plt.figure(figsize=(8, 6))
+    plt.plot(bin_edges[0:-1], histogram)  # Plot the histogram
+    plt.title('Grayscale Histogram for Segmented Person')
+    plt.xlabel('Pixel Intensity')
+    plt.ylabel('Pixel Count')
+    plt.show()
